e699ef366c263ee5da3dbe8a28d1eb48c80eb89b
dennis <dennis34@students.cs.ubc.ca>
2022-03-31 01:29:22 -0700

    home screen

---
+++ src/portfolioContainer/home/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { TOTAL_SCREENS, GET_SCREEN_INDEX } from "../../../utilities/commonUtils";
+import ScrollService from "../../../utilities/ScrollService";  
+import {faBars} from @fortaw
+
+export default function Header() {
+  return <div></div>;
+}
